ab95cf7d51752d7b0d9c364ed2814d8ff059a857
empmaynmtusrgdyt <leammax@yandex.ru>
2026-04-26 17:22:30 +0300

    29 commit: fix bug in scripts/build-and-run-win.bat

--- scripts/build-and-run-win.bat
+++ scripts/build-and-run-win.bat
@@ -2,6 +2,6 @@
 cd /d "%~dp0\.."
 gcc scripts\parser.c src\cJSON.c src\sqlite3.c -Isrc -o scripts\parser_c.exe
 scripts\parser_c.exe
-gcc src\main.c src\sqlite3.c src\cJSON.c -o -Isrc mini-file-command.exe
+gcc src\main.c src\sqlite3.c src\cJSON.c -Isrc -o mini-file-command.exe
 echo Сборка завершена. Запускайте программу командой: mini-file-command.exe [FILE]
 pause
